51c320557de0739a61a3670ffe13d8dfe9a4c485
ToluAkinlabi <tolulopeolumide.to@gmail.com>
2024-09-21 16:28:51 -0400

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import 'bootstrap/dist/css/bootstrap.css';
 import React from "react";
-import logo from './phoenix.png'
+import logo from './phoenix.png';
+import './page.css';
  
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Image src={logo} alt="logo" />
+    <div>
+      <main>
+        <navbar className="navbar nav bg-body-tertiary">
+            <Image src={logo} alt="logo" width="30" height="30"/>Team Phoenix
+        </navbar>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
         We are Team Phoenix{" "}.
